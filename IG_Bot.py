bf557265cca3e43d569da1c5b740e17978074c58
Kianmhz <kianmehr1777@gmail.com>
2023-08-02 14:00:57 -0400

    Added uploading file for story functionality.

--- IG_Bot.py
+++ IG_Bot.py
@@ -6,8 +6,11 @@ from selenium.webdriver.chrome.options import Options
 import os
 import time
 
+width = 375
+height = 812
+
 mobile_emulation = {
-    "deviceMetrics": { "width": 375, "height": 812, "pixelRatio": 3.0 },
+    "deviceMetrics": { "width": width, "height": height, "pixelRatio": 3.0 },
     "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1"
 }
 
@@ -24,7 +27,7 @@ driver.get("https://www.instagram.com/accounts/login/")
 
 # Checking if we are on the login page by examining the URL
 if "accounts/login" in driver.current_url:
-
+    login = True
     username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div[1]/div[3]/div/label/input')))
     password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div[1]/div[4]/div/label/input')))
     login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div[1]/div[6]')))
@@ -33,11 +36,17 @@ if "accounts/login" in driver.current_url:
     password_field.send_keys(password)
     login_button.click()
 
+    not_now_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@role="button" and text()="Not Now"]')))
+    not_now_button.click()
 
-# input_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="mount_0_0_Hl"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/section/div/div[2]/div/div/div/div[1]/button/form/input')))
 
-# file_path = "C:\\Users\\kianm\\OneDrive\\Pictures\\IMG_0045.JPG" # Path to the file you want to upload
+# Absolute path to the file you want to upload
+file_path = "C:\\Users\\kianm\\OneDrive\\Desktop\\Python\\RestaurantFinder\\static\\img\\rest_6.jpeg"
 
-# input_field.send_keys(file_path)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div/div[1]/div[1]/div[2]/section/div[1]/nav/div/header/div/div/div[1]/div[1]/span/div/a/div'))).click()
+
+story = driver.find_element(By.XPATH, '//input[@accept="image/jpeg,image/png"]')
+story.send_keys(file_path)
+
 
 time.sleep(600)
