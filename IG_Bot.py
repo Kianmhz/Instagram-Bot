2948e7c41b92bb25118bf4f10a14aa756bac856e
Kianmhz <kianmehr1777@gmail.com>
2023-08-06 13:58:01 -0400

    Added story functionality.

--- IG_Bot.py
+++ IG_Bot.py
@@ -17,7 +17,7 @@ with sync_playwright() as p:
         user_agent="Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1",
     )
 
-    page = browser.new_page()
+    page = context.new_page()
 
     if Path("cookies.json").exists():
         page.context.add_cookies(json.loads(Path("cookies.json").read_text()))  # load cookies if they exist
@@ -40,9 +40,10 @@ with sync_playwright() as p:
 
         cookies = page.context.cookies() 
         Path("cookies.json").write_text(json.dumps(cookies)) # save cookies to file if login was successful
-    else:
-        page.wait_for_selector('button:text("Not Now")').click()
 
+
+    story = page.wait_for_selector('div:text("Your story")')
+    story.click()
 
 
     time.sleep(500)
