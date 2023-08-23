6e1a717dadbd51a1809bc365c5be01635a40a4b8
Kianmehr Haddad <haddad.kianm@gmail.com>
2023-08-23 13:36:50 -0400

    Added follow and dataset function(not working)

--- IG_Bot.py
+++ IG_Bot.py
@@ -1,14 +1,18 @@
 from playwright.sync_api import sync_playwright
 import os
 import time
+import requests
+from dotenv import load_dotenv
+from random import choice
 
+load_dotenv()
 
 USERNAME = os.getenv('MY_USERNAME')  # replace with your environment variable for username
 PASSWORD = os.getenv('MY_PASSWORD')  # replace with your environment variable for password
 
 with sync_playwright() as p:
     context = p.chromium.launch_persistent_context(
-        user_data_dir="C:\\Users\\kianm\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 5",
+        user_data_dir="/Users/kianmhz/Library/Application Support/Google/Chrome/Profile 2",
         headless=False,
     )
 
@@ -28,7 +32,7 @@ with sync_playwright() as p:
         page.click("button[type='submit']")
 
         try:
-            page.wait_for_selector('div:text("Not Now")').click()
+            page.wait_for_selector('div:has-text("Not Now")').click()
         except:
             print("login failed")
 
@@ -40,7 +44,31 @@ with sync_playwright() as p:
 
         page.set_input_files('xpath=//input[@class="_ac69" and @type="file" and @accept="image/jpeg,image/png,image/heic,image/heif,video/mp4,video/quicktime"]', file_path)
 
-    upload_photo("C:\\Users\\kianm\\Desktop\\test.jpg")   # replace with your file path
+    # upload_photo("C:\\Users\\kianm\\Desktop\\test.jpg")   # replace with your file path
+
+    def dataset():
+        page.goto('https://t.me/meme/756')
+        time.sleep(5)
+        link_element = page.wait_for_selector('xpath=/html/body/div/div[2]/a[2]')
+        print(link_element)
+        image_style = link_element.get_attribute('style')
+        image_url = image_style.split("url('")[1].split("')")[0]
+
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open('image.jpg', 'wb') as file:
+                file.write(response.content)
+        else:
+            print('Failed to download the image')
+    
+    def follow():
+         # Will randomly pick one of these below sources and then follow their n last followers
+        follow_id_list = ["username1", "username2"]  # Replace with your list of usernames
+        random_number = choice(range(len(follow_id_list)))
+
+        page.goto(f'https://www.instagram.com/{follow_id_list[random_number]}/followers/')
+
+    follow()
 
 
     time.sleep(500)
